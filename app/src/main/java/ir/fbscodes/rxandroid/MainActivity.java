88425fcae92f7189d0113f45d15d193da6ee0247
saeid-hanafi <saeed.hanafi.ciw@gmail.com>
2022-11-09 11:44:17 +0330

    run-observable-on-loopers

--- app/src/main/java/ir/fbscodes/rxandroid/MainActivity.java
+++ app/src/main/java/ir/fbscodes/rxandroid/MainActivity.java
@@ -3,12 +3,53 @@ package ir.fbscodes.rxandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Disposable disposable;
+    private static final String TAG = "MainActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just("tiger", "lion", "bear", "snack")
+                .observeOn(AndroidSchedulers.from(Looper.getMainLooper()))
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Log.i(TAG, "onNext: ");
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.e(TAG, "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete: ");
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
     }
 }
