ce695b0e29a896534fc53d662f7bf53c54f8bc6b
saeid-hanafi <saeed.hanafi.ciw@gmail.com>
2022-11-12 16:06:51 +0330

    test-subjects-types-and-operators-on-rxjava

--- app/src/main/java/ir/fbscodes/rxandroid/MainActivity.java
+++ app/src/main/java/ir/fbscodes/rxandroid/MainActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.schedulers.Timed;
+import io.reactivex.rxjava3.subjects.BehaviorSubject;
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import io.reactivex.rxjava3.subjects.ReplaySubject;
 
 public class MainActivity extends AppCompatActivity {
     private Disposable disposable;
@@ -21,35 +31,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable.just("tiger", "lion", "bear", "snack")
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-                        disposable = d;
-                    }
+//        createRangeObservable()
+//                .timestamp()
+//                .subscribeOn(Schedulers.newThread())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Observer<Timed<Integer>>() {
+//                    @Override
+//                    public void onSubscribe(@NonNull Disposable d) {
+//                        disposable = d;
+//                    }
+//
+//                    @Override
+//                    public void onNext(@NonNull Timed<Integer> s) {
+//                        Log.i(TAG, "onNext: "+ s);
+//                    }
+//
+//                    @Override
+//                    public void onError(@NonNull Throwable e) {
+//                        Log.e(TAG, "onError: " + e.getMessage());
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//                        Log.i(TAG, "onComplete: ");
+//                    }
+//                });
 
-                    @Override
-                    public void onNext(@NonNull String s) {
-                        Log.i(TAG, "onNext: ");
-                    }
+        BehaviorSubject<Integer> subject = BehaviorSubject.create();
+        subject.onNext(1);
+        subject.onNext(2);
+        subject.onNext(3);
 
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Log.e(TAG, "onError: " + e.getMessage());
-                    }
+        subject.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                disposable = d;
+            }
 
-                    @Override
-                    public void onComplete() {
-                        Log.i(TAG, "onComplete: ");
-                    }
-                });
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.i(TAG, "onNext: "+ integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        subject.onNext(4);
+        subject.onNext(5);
+        subject.onNext(6);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         disposable.dispose();
+    }
+
+    private Observable<Integer> createRangeObservable() {
+        return Observable.create(emitter -> {
+            try {
+                for (int i = 0; i < 15; i++) {
+                    if (!emitter.isDisposed()) {
+                        emitter.onNext(i);
+                    }
+                }
+
+                if (!emitter.isDisposed()) {
+                    emitter.onComplete();
+                }
+            }catch (Exception e) {
+                if (!emitter.isDisposed()) {
+                    emitter.onError(e);
+                }
+            }
+        });
     }
 }
